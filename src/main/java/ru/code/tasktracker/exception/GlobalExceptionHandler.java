729515eb8cfe92482a1781c9895e8f7eb2f3378c
Mm <maxkulezenin@gmail.com>
2026-04-16 22:35:08 +0700

    processing new exceptions by GlobalExceptionHandler

--- src/main/java/ru/code/tasktracker/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/code/tasktracker/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.code.tasktracker.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,24 +12,26 @@ public class GlobalExceptionHandler
 {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> handleBadRequest(BadRequestException e)
-    {
-        log.warn("Bad request: {}", e.getMessage());
-        return ResponseEntity.status(400).body(e.getMessage());
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleRequiredResultNotFound(IllegalArgumentException e)
-    {
-        log.warn("Required result not found: {}", e.getMessage());
-        return ResponseEntity.status(404).body(e.getMessage());
-    }
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception e)
+    public ResponseEntity<String> handleAnyException(Exception e)
     {
         log.error("Handle exception ", e);
+        return ResponseEntity.status(500).body(e.getMessage());
+    }
+
+    @ExceptionHandler(exception = {
+            IllegalArgumentException.class,
+            ValidationFailedException.class})
+    public ResponseEntity<String> handleInvalidRequestException(Exception e)
+    {
+        log.warn("Invalid request: {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ValidationFailedException.class)
+    public ResponseEntity<String> handleDataProcessingException(Exception e)
+    {
+        log.warn("Processing error: {}", e.getMessage());
         return ResponseEntity.status(500).body(e.getMessage());
     }
 
